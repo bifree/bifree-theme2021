d7be8c0f2a3eb7f870d04505807439bf558b440f
bifree <bifree2bfree@gmail.com>
2021-09-20 03:06:32 +0900

    about更新

---
+++ page-about.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/reset.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/style.css">
+</head>
+<body>
+	<div class="container">
+		<div class="page__header">
+			<h2 class="upper--table">組織概要</h2>
+		</div>
+		<div class="intro__table">
+			<table>
+				<tbody>
+					<tr>
+						<td>組織名</td>
+						<td>Bifree</td>
+					</tr>
+					<tr>
+						<td>設立</td>
+						<td>2021年２月</td>
+					</tr>
+					<tr>
+						<td>代表</td>
+						<td>相原将希</td>
+					</tr>
+					<tr>
+						<td>事業内容</td>
+						<td>
+							・ウェブサイト構築事業<br>
+							・ウェブシステム構築事業<br>
+							・ウェブメディア運営事業<br>
+							・プログラマ育成事業<br>
+							・LGBTQに関する啓蒙活動
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<div class="contact--to">
+			<h2>お問い合わせはこちらから</h2>
+			<div class="contact">Contact</div>
+		</div>
+	</div>
+</body>
+</html>
