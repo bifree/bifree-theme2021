dfc1c8ba4fded2a3aeb82be0ceea6b761934a87c
bifree <bifree2bfree@gmail.com>
2021-09-21 23:02:24 +0900

    固定ページにヘッダーフッター追加

--- page-about.php
+++ page-about.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/reset.css">
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/style.css">
-</head>
+<?php get_header(); ?>
 <body>
 	<div class="container">
 		<div class="page__header">
@@ -47,4 +38,4 @@
 		</div>
 	</div>
 </body>
-</html>
+<?php get_footer(); ?>
--- page-service.php
+++ page-service.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/reset.css">
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/style.css">
-</head>
-<body>
+<?php get_header(); ?>
 	<div class="container about__cont">
 	<div class="page__header">
 			<h2 class="upper--table">事業紹介</h2>
@@ -135,4 +125,4 @@
 
 	</div>
 </body>
-</html>
+<?php get_footer(); ?>
