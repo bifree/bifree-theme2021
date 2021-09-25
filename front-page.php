4152b1c858066ae9262a25300b495afa9f0e3609
bifree <bifree2bfree@gmail.com>
2021-09-25 17:40:14 +0900

    本番用css

---
+++ front-page.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/reset.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/style.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/responsive.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined">
+    <?php wp_head(); ?>
+</head>
+<div class="container center">
+	<img src="<?php echo get_template_directory_uri(); ?>/img/bifree_icon.jpg" alt="">
+	<h1 class="renewal_h1">ただいまリニューアル中です</h1>
+</div>
