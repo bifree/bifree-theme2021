764e2223af8b6658a123dd72673cca18278fa85d
bifree <bifree2bfree@gmail.com>
2021-09-27 01:33:31 +0900

    about編集

--- page-about.php
+++ page-about.php
@@ -1,19 +1,8 @@
 <?php get_header(); ?>
 <body>
 	<div class="container page--top">
-	<section class="typeA">
-	<input id="TAB-A01" type="radio" name="TAB-A" checked="checked">
-	<label class="tabLabel" for="TAB-A01">Bifreeについて</label>
-	<div class="content">
 		<?php get_template_part('template/about-bifree');?>
-	</div>
-	<input id="TAB-A02" type="radio" name="TAB-A">
-	<label class="tabLabel" for="TAB-A02">組織概要図</label>
-	<div class="content">
 		<?php get_template_part('template/gaiyou');?>
-	</div>
-</section>
-		
 	</div>
 </body>
 <?php get_footer(); ?>
