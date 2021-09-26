20760d58a4dc8574b5ba1ce1ab6f354e243e1bbc
michi <michiko@michimac.local>
2021-09-26 12:12:17 +0900

    サービスの動き追加

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//メイン キャッチコピー
 $(function(){
   var scrollEnd = $('.about').offset().top; //ページ上部からの距離を取得
   var distance = 0;
@@ -15,4 +16,21 @@ $(function(){
       //『.about』より上部に戻ったらclass『fixed』を追加
     }
   });      
+});
+
+
+//メイン サービス
+$(function () {
+  $(window).scroll(function () {
+    $(".services").each(function () {
+      var elemPos = $(this).offset().top; //要素の位置を取得 
+      var scroll = $(window).scrollTop(); //スクロール位置を取得 */
+      var windowHeight = $(window).height(); //画面幅を取得（画面の下側に入ったときに動作させるため） 
+      if (scroll > elemPos - windowHeight) {
+        //要素位置までスクロール出来たときに動作する 
+        $(this).addClass("services-scroll");
+      }
+    });
+  });
+  jQuery(window).scroll();
 });
