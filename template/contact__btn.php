31c01c451d4328208bf0e6cf3a6dfee5e676c624
bifree <bifree2bfree@gmail.com>
2021-09-26 18:11:37 +0900

    css編集

--- template/contact__btn.php
+++ template/contact__btn.php
@@ -1,4 +1,4 @@
-<a class="contact__btn" href="<?php echo home_url('/contact/');?>">
+<a class="contact__btn1" href="<?php echo home_url('/contact/');?>">
 	お問い合わせはこちら
 	<img class="contact__btn__img" src="<?php echo get_template_directory_uri(); ?>/assets/img/mail.png">
 	<img class="contact__btn__arrow" src="<?php echo get_template_directory_uri(); ?>/assets/img/contact_arrow.png"> 
