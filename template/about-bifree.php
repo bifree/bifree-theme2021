0809e571b3d2bd9980bca996ab4d4f778ebdd3ec
bifree <bifree2bfree@gmail.com>
2021-09-30 23:44:42 +0900

    写真

--- template/about-bifree.php
+++ template/about-bifree.php
@@ -6,16 +6,19 @@
 		<h1 class="about__h1">代表挨拶</h1>
 		<div class="greet__top">
 			<div class="greet__pic">
-				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/pic.png" alt="">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/greeting.jpg" alt="">
 			</div>
 			<div class="greet__text">
 				<p class="text--justify">「世界のみんなが自由に生きられたらいい」そんな理想は現実とはとても乖離したものです。実際には経済的格差があり、生まれた場所の地理的要因や宗教上の理由等で全ての人が自分の望む自由を手に入れることはできない。だからこそ、私も含め人はみんな自由を夢見て、恋焦がれるのだと思います。<br>
 				LGBTQ当事者も例に漏れず、自由に恋愛したり堂々と自分らしく生活出来ることを望む人が多くいます。しかし閉鎖的な環境や、他者からの目に怯えて、自分らしく振る舞えなかったり、社会にうまく馴染めず経済的に困窮し目の前のことにいっぱいいっぱいな人もいるのです。
 				私たちBifreeは、そんな彼らの自由のきっかけになりたい。彼らが私たちの活動を見て、勇気を得たり、元気になったり、笑顔になったり（ラジバンダリ）。その笑顔や勇気は、未来へ繋がるアクションや人の輪を呼び集め、きっと彼ら自身の誇りとなるでしょう。誇りの先に自由が、あることを信じて。これからも私たちは、あなたに寄り添います。</p>
 			</div>
+			<div class="signature">
+			<img src="<?php echo get_template_directory_uri(); ?>/assets/img/signature.png" alt="">
+			</div>
 		</div>
 		<h1 class="about__h1">メンバー紹介</h1>
-		<p class="service__body text--justify">Bifreeで日々活動しているメンバーの声を紹介します！</p>
+		<p class="text--justify">Bifreeで日々活動しているメンバーの声を紹介します！</p>
 		<div class="member__voice member__voice--1st">
 			<div class="member__voice--pic">
 				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/member_image1.png" alt="">
