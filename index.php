97c3cfa43db1031ca49909339f4c1976d8e8eac2
michi <michiko@michimac.local>
2021-10-01 02:54:56 +0900

    レスポンシブ編集

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php get_header(); ?>
 <main>
     <div class="main-visual">
-        <img class="main-visual__img1" src="<?php echo get_template_directory_uri(); ?>/assets/img/main-visual1.png"
-            alt="トップ画像">
-        <img class="main-visual__img2" src="<?php echo get_template_directory_uri(); ?>/assets/img/main-visual2.png"
-            alt="トップ画像">
+        <img class="main-visual__img1 main-visual__pc" src="<?php echo get_template_directory_uri(); ?>/assets/img/main-visual1.png" alt="トップ画像">
+        <img class="main-visual__img2 main-visual__pc" src="<?php echo get_template_directory_uri(); ?>/assets/img/main-visual2.png" alt="トップ画像">
+
+        <img class="main-visual__img1__sp main-visual__sp" src="<?php echo get_template_directory_uri(); ?>/assets/img/main-visual1sp.png" alt="トップ画像">
+        <img class="main-visual__img2__sp main-visual__sp" src="<?php echo get_template_directory_uri(); ?>/assets/img/main-visual2sp.png" alt="トップ画像">
+            
         <div class="catch-copy">
           <div class="catch-copy__ja">
             “あなたらしさ”  <span class="wo">を</span><br>
