d9c632f38dfc315683b75351b566c2acd5eda4d0
michi <michiko@michimac.local>
2021-10-04 21:46:53 +0900

    archive.php編集

--- functions.php
+++ functions.php
@@ -16,3 +16,14 @@ add_action('qp_enqueue_scripts','add_stylesheet');
 // カスタムメニュー設定
 register_nav_menu( 'header-nav', ' ヘッダーナビゲーション ' );
 register_nav_menu( 'footer-nav', ' フッターナビゲーション ' );
+
+// archive.php 設定
+function post_has_archive($args, $post_type)
+{
+    if ('post' == $post_type) {
+        $args['rewrite'] = true; // リライトを有効にする
+        $args['has_archive'] = 'article'; // 任意のスラッグ名
+    }
+    return $args;
+}
+add_filter('register_post_type_args', 'post_has_archive', 10, 2);
